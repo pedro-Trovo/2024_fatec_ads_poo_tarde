201f216cc17dfc8d704e95112aba21139c2d4af6
Pedro Trovo <trovopedro5@gmail.com>
2024-03-14 16:01:12 -0300

    feat(ap006): implementa a calculadora
    -faz as quatro operacoes matematicas
    -intuito era estudar metodos

---
+++ calculadora.java
@@ -0,0 +1,27 @@
+public class calculadora {
+    //tipo de retorno
+    //nome
+    //lista de parametros
+    //corpo
+    int somar(int n1, int n2){
+        int res = n1 + n2;
+        System.out.println(res);
+        return res;
+    }
+    int subtrair(int n1, int n2){
+        int res = n1 - n2;
+        System.out.println(res);
+        return res;
+    }
+    int multiplicar(int n1, int n2){
+        int res = n1 * n2;
+        System.out.println(res);
+        return res;
+    }
+    int dividir(int n1, int n2){
+        int res = n1 / n2;
+        System.out.println(res);
+        return res;
+    }
+
+}
