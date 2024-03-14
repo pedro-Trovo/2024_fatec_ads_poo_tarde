93a29ac947889cbc0f8f700236633d97012a4ade
Pedro Trovo <trovopedro5@gmail.com>
2024-03-14 16:11:54 -0300

    test(006): testa construtor padrao

---
+++ testelivrodenotas.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+public class testelivrodenotas {
+
+    public static void main(String[] args){
+      //  var livrodenotas = new LivroDeNotas();
+       // var livrodenotas2 = new LivroDeNotas();
+        String nomeDaDisciplina = JOptionPane.showInputDialog("Prof, Qual o nome da disciplina1");
+       // livrodenotas.nomeDaDisciplina = nomeDaDisciplina;
+        nomeDaDisciplina = JOptionPane.showInputDialog("Prof, Qual o nome da disciplina2");
+       //livrodenotas2.nomeDaDisciplina = nomeDaDisciplina;
+       // livrodenotas.exibirMensagem();
+       // livrodenotas2.exibirMensagem();
+    }
+    
+}
