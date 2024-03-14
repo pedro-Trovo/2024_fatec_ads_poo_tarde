046bcf255a9edc822acbc5a06688ce26b3a11ab0
Pedro Trovo <trovopedro5@gmail.com>
2024-03-14 16:16:12 -0300

    test(ap006):testa getters/setters e construtor

---
+++ TesteLivroDeNotasV2.java
@@ -0,0 +1,8 @@
+public class TesteLivroDeNotasV2 {
+    public static void main(String[] args){
+     //inferencia de tipo
+    var meuLivro = new LivroDeNotas("POO");
+    //meuLivro.setNomeDaDisciplina = "POO";
+    System.out.println(meuLivro.getNomeDaDisciplina());
+    } 
+}
